5764eab4903f77a2c15055190f7c34d5848467d0
padmapriya <padmapriya@padmapriyas-MacBook-Pro.local>
2021-03-12 23:12:06 +0530

    Product Application Test

---
+++ src/test/java/com/target/product/ProductApplicationTests.java
@@ -0,0 +1,15 @@
+package com.target.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductApplicationTests {
+	@Test
+	public void contextLoads() {
+		
+	}
+
+	
+
+}
